8b37b81403bba9118f7e488fce052f5f86030d99
Med Ayed <115380260+Smaaks@users.noreply.github.com>
2024-10-26 22:52:31 +0100

    upt

--- gestion-station-ski/src/test/java/tn/esprit/spring/service/SkiersServiceImplTest.java
+++ gestion-station-ski/src/test/java/tn/esprit/spring/service/SkiersServiceImplTest.java
@@ -3,9 +3,11 @@ package tn.esprit.spring.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.entities.*;
 import tn.esprit.spring.repositories.*;
 import tn.esprit.spring.services.SkierServicesImpl;
@@ -18,7 +20,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class SkierServicesImplTest {
 
     @Mock
